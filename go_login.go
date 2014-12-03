e5e5ae2b02a0f4a3175f6878fc6397b4d7c172f1
Morgan Hill <admin@pcwizz.net>
2014-12-03 22:01:18 +0000

    Started again!

--- go_login.go
+++ go_login.go
@@ -1,121 +1,239 @@
 package go_login
 
 import (
-	//"database/sql",
-	//"github.com/go-sql-driver/mysql"
+	"log"
 	"time"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
+	"crypto/rand"
 )
 
-// Email address
+var database *sql.DB
 
-//They can be active or in active not just strings
-type email struct {
-	Email string
-	Active bool
-	User *user //Doubling up the links for ease of use.
-}
-//A user may have more than one
-type emails []email;
-func activeEmails_h(es emails) emails{
-	output := make(emails,0, len(es))
-	for i := range es {
-		if (es[i].Active){
-			output = append(output, es[i])
-		}
+func Init(connectString string){
+	db, err := sql.Open("mysql", connectString)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return output
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	database = db
+	//Set up tables
+	_, err = database.Exec("create table if not exist users " +
+		"(id unsigned integer primary key auto_increment," +
+		" name text not null, email text not null," +
+		" password text, salt text, hash text)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = database.Exec("create table if not exist tokens " +
+		"(id unsigned integer primary key auto_increment," +
+		" issued bigint, expires bigint, userID unsigned integer)")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-//We only care about active emails most of the time
-func activeEmails(es emails)(emails){
-	length := len(es)
-	switch length{
-		case 0:
-			return nil
-		case 1:
-			if es[0].Active {
-				return es
-			} else {
-				return nil	
+
+func Uninit(){
+	database.Close()
+}
+
+type Password struct {
+	Password string//The hashed password
+	Salt string//The pseudo-random string used to mitigate dictionary attacks
+	Hash string//The cryptographic hashing algorithm used
+}
+
+func Authenticate(id uint, password string) bool {
+	pswdstmt, err := database.Prepare("select password, salt, hash, id from users where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pswdstmt.Close()
+	var pswd Password
+	err = pswdstmt.QueryRow(id).Scan(&pswd.Password, &pswd.Salt, &pswd.Hash)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false//doesn't exist.
+		}
+		log.Fatal(err)
+	}
+	var pswdInput []byte
+	switch pswd.Hash {
+		case "bcrypt20"://bcrypt with a cost of 20
+			pswdInput, err = bcrypt.GenerateFromPassword([]byte(password + pswd.Salt), 20)
+			if err != nil {
+				log.Fatal(err)
 			}
 		default:
-			return activeEmails_h(es)
+			log.Println("Didn't recognize hash")
+			return false
+	}
+	if pswd.Password == string(pswdInput) {
+		return true
+	}
+	return false
+}
+
+type User struct {
+	Id uint 
+	Name string
+	Email string
+}
+
+func GetUser(id uint) User {
+	var user User
+	if id == 0 {
+		return user;
+	}
+	userstmt, err := database.Prepare("select name, email from users where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer userstmt.Close()
+	if err = userstmt.QueryRow(id).Scan(&user.Name, &user.Email); err != nil {
+		if err != sql.ErrNoRows {
+			log.Panic(err)
+		}
+		return user
+	}
+	user.Id = id
+	return user
+}
+
+func UpdateName(id uint, name string) {
+	if id == 0 {
+		return	
+	}
+	updtstmt, err := database.Prepare("update users set name = ? where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer updtstmt.Close()
+	if _, err := updtstmt.Exec(name, id); err != nil {
+		log.Fatal(err)
 	}
 }
 
-
-//Passwords
-//We need to know which cryptographic hashing algorithm we used,
-//because it will change as computers get more powerful, so we
-//need to think ahead.
-//We also need to know the salt; salt makes everything better 
-//(apart from human health).
-//We don't like manipulating strings when we don't have to.
-type password struct {
-	Password string
-	Salt string
-	Hash string
+func UpdateEmail(id uint, email string) {
+	if id == 0 {
+		return	
+	}
+	updtstmt, err := database.Prepare("update users set email = ? where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer updtstmt.Close()
+	if _, err := updtstmt.Exec(email, id); err != nil {
+		log.Fatal(err)
+	}
 }
 
-//Permissions
-//We could just use an enum, but then we would have to change this 
-//for just about every application we write using it; I'm lazy.
-type permission struct {
-	Id int 
-	Description string
+func UpdatePassword (id uint, password string) {
+	if id == 0 {
+		return
+	}
+	//Make a salt
+	salt := make([]byte, 128)//Allocate the memory
+	_, err := rand.Read(salt)//Populate with random
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Append onto password
+	password = password + string(salt)
+	//Generate the hash
+	hashedpswd, err := bcrypt.GenerateFromPassword([]byte(password), 20)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Update the password in the database
+	stmt, err := database.Prepare("update users set password = ?, salt = ?, hash = ? where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	if _, err := stmt.Exec(hashedpswd, string(salt), "bcrypt20", id); err != nil {
+		log.Fatal(err)
+	}
 }
 
-type permissions []permission
-
-//Groups
-//Modelling the inheritance hirarchy of our groups in code is silly,
-//we are more concerned about cpu time than memory, the database can
-//provide abstraction; the database will do a better job than me.
-
-type group struct {
-	Name string
-	Description string
-	Permissions permissions
+func CreateUser (user User, password string) uint {
+	stmt, err := database.Prepare("insert into users (name, email) values (?, ?)")//Going to use update password
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(user.Name, user.Email)
+	if err != nil {
+		log.Fatal(err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	go UpdatePassword(uint(id), password)
+	return uint(id)
 }
 
-type groups []group
-
-//Tokens
-//For ****'s sake give them expiry dates, then check the hole thing.
-//We don't give a **** about when we gave out a token, I put it in 
-//the db for a clear audit trail.
-type token struct {
+type Token struct {
+	Id uint
 	Token string
-	Expires time.Time
+	Issued int64
+	Expiry int64
+	UserID uint
 }
 
-//Users
-type user struct {
-	Id int
-	Username string
-	Password password
-	Active bool
-	Emails emails
-	Groups groups //users can have more than one group; might be useful.
+func IssueToken (userID uint, duration int64) Token {
+	var token Token
+	token.UserID = userID
+	now := time.Now().Unix()
+	token.Issued = now
+	token.Expiry = now + duration
+	//Generate token
+	tokenStr := make([]byte, 128)
+	if _, err := rand.Read(tokenStr); err != nil {
+		log.Fatal(err)
+	}
+	token.Token = string(tokenStr)
+	//Add token to database
+	stmt, err := database.Prepare("insert into tokens (token, issued, expiry, userID) values (?,?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(token.Token, token.Issued, token.Expiry, token.UserID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	token.Id = uint(id)
+	return token
 }
 
-//Sessions (login tokens)
-type session struct {
-	User *user
-	Token token
+func Login (email string, password string) Token {
+	//Find the user
+	stmt, err := database.Prepare("select id from users where email = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var id uint
+	if err = stmt.QueryRow(email).Scan(&id); err != nil {
+		if err == sql.ErrNoRows {
+			var token Token 
+			return token//Return an empty token
+		}
+		log.Fatal(err)
+	}
+	if Authenticate(id, password) {
+		return IssueToken(id, 60*60*2)//Give a 2 hour token
+	}
+	var token Token
+	return token
 }
-
-type sessions []session
-
-
-//Login attempts
-//Keeping track of failed logins so attacks can be mitigated
-
-type attempt struct {
-	Host string
-	Time time.Time
-	Email email //Who did they try to log in ass, do they even exist
-} 
-
-//We will want to keep a time based buffer of these
-
-
